5c21fbd0fae747875c02aad256c3b11467625e00
Chandrakala Subramanyam <csubrama@redhat.com>
2022-07-13 20:48:25 +0530

    Moved common code & fixed few test failures

--- tests/scripts/common/common_functions.sh
+++ tests/scripts/common/common_functions.sh
@@ -549,3 +549,153 @@ function verify_grpc_result() {
 	fi
 }
 
+# The test does the following:
+# In case of hpo_post_experiment test, Post valid and invalid experiments to HPO /experiment_trials API and validate the reslut
+# In case of hpo_post_exp_result test, Post valid and invalid experiments results to HPO /experiment_trials API and validate the result
+# input: Test name
+function run_post_tests(){
+	hpo_test_name=$1
+	
+	if [ "${hpo_test_name}" == "hpo_post_experiment" ]; then
+		exp_tests=("${run_post_experiment_tests[@]}")
+	else
+		exp_tests=("${run_post_exp_result_tests[@]}")
+	fi
+
+	SERV_LOG="${TEST_DIR}/service.log"
+	# Deploy hpo
+	if [ ${cluster_type} == "native" ]; then
+		deploy_hpo ${cluster_type} ${SERV_LOG}
+	else
+		deploy_hpo ${cluster_type} ${HPO_CONTAINER_IMAGE} ${SERV_LOG}
+	fi
+	
+	# Check if HPO services are started
+	check_server_status
+
+	for post_test in "${exp_tests[@]}"
+	do
+
+		# Get the length of the service log before the test
+		log_length_before_test=$(cat ${SERV_LOG} | wc -l)
+
+		TESTS_="${TEST_DIR}/${post_test}"
+		mkdir -p ${TESTS_}
+		LOG_="${TEST_DIR}/${post_test}.log"
+		TEST_SERV_LOG="${TESTS_}/service.log"
+
+		echo "************************************* ${post_test} Test ****************************************" | tee -a ${LOG_} ${LOG}
+		echo "" | tee -a ${LOG_} ${LOG}
+
+		exp="${post_test}"	
+
+		experiment_name=""
+		if [ "${hpo_test_name}" == "hpo_post_exp_result" ]; then
+			exp="valid-experiment"
+			# Get the experiment id from search space JSON
+			experiment_name=$(echo ${hpo_post_experiment_json[${exp}]} | jq '.search_space.experiment_name')
+			# Post the experiment JSON to HPO /experiment_trials API
+			post_experiment_json "${hpo_post_experiment_json[${exp}]}"
+
+			post_exp_http_code="${http_code}"
+
+			# Post the experiment result to HPO /experiment_trials API
+			post_experiment_result_json "${hpo_post_exp_result_json[$post_test]}"
+			expected_log_msg="${hpo_exp_result_error_messages[$post_test]}"
+		else
+			# Get the experiment id from search space JSON
+			experiment_name=$(echo ${hpo_post_experiment_json[${post_test}]} | jq '.search_space.experiment_name')
+			# Post the experiment JSON to HPO /experiment_trials API
+			post_experiment_json "${hpo_post_experiment_json[$post_test]}"
+
+			expected_log_msg="${hpo_error_messages[$post_test]}"
+
+			post_exp_http_code="${http_code}"
+		fi
+
+		if [[ "${post_test}" == valid* ]]; then
+			expected_result_="200"
+		else
+			expected_result_="400"
+			if [[ "${post_test}" == "generate-subsequent" ]]; then
+				expected_result_="404"
+			fi
+		fi
+
+		actual_result="${http_code}"
+
+		should_stop_expriment=false
+		if [[ "${post_exp_http_code}" -eq "200" ]]; then
+			should_stop_expriment=true
+		fi
+
+		# Extract the lines from the service log after log_length_before_test
+		extract_lines=`expr ${log_length_before_test} + 1`
+		cat ${SERV_LOG} | tail -n +${extract_lines} > ${TEST_SERV_LOG}
+		
+		echo ""
+		echo "log_length_before_test ${log_length_before_test}"
+		echo "extract_lines ${extract_lines}"
+		echo ""
+
+		echo ""
+		if [[ "${http_code}" -eq "000" ]]; then
+			failed=1
+			((TOTAL_TESTS++))
+			((TESTS++))
+			error_message "${failed}" "${post_test}"
+		else
+			echo "actual_result = $actual_result expected_result = ${expected_result_}"
+			compare_result "${post_test}" "${expected_result_}" "${expected_log_msg}" "${TEST_SERV_LOG}"
+		fi
+
+		echo ""
+
+		if [ "$should_stop_expriment" == true ]; then
+			stop_experiment "$experiment_name"
+		fi
+
+		echo "" | tee -a ${LOG_} ${LOG}
+		
+	done
+	
+	# Stop the HPO servers
+	echo "Terminating any running HPO servers..." | tee -a ${LOG}
+	terminate_hpo ${cluster_type}
+	echo "Terminating any running HPO servers...Done" | tee -a ${LOG}
+
+	echo "*********************************************************************************************************" | tee -a ${LOG_} ${LOG}
+}
+
+# Post a JSON object to HPO(Hyper Parameter Optimization) module
+# input: JSON object
+# output: Create the Curl command with given JSON and get the result
+function post_experiment_json() {
+	json_array_=$1
+	echo ""
+	echo "******************************************"
+	echo "json array = ${json_array_}"
+	echo "******************************************"
+
+	form_hpo_api_url "experiment_trials"
+
+	post_cmd=$(curl -s -H 'Content-Type: application/json' ${hpo_url}  -d "${json_array_}"  -w '\n%{http_code}' 2>&1)
+
+	# Example curl command: curl -v -s -H 'Content-Type: application/json' http://localhost:8085/experiment_trials -d '{"operation":"EXP_TRIAL_GENERATE_NEW","search_space":{"experiment_name":"petclinic-sample-2-75884c5549-npvgd","experiment_id":"a123","value_type":"double","hpo_algo_impl":"optuna_tpe","objective_function":"transaction_response_time","tunables":[{"value_type":"double","lower_bound":150,"name":"memoryRequest","upper_bound":300,"step":1},{"value_type":"double","lower_bound":1,"name":"cpuRequest","upper_bound":3,"step":0.01}],"slo_class":"response_time","direction":"minimize"}}' 
+
+	post_experiment_cmd="curl -s -H 'Content-Type: application/json' ${hpo_url} -d '${json_array_}'  -w '\n%{http_code}'"
+
+	echo "" | tee -a ${LOG_} ${LOG}
+	echo "Curl command used to post the experiment = ${post_experiment_cmd}" | tee -a ${LOG_} ${LOG}
+	echo "" | tee -a ${LOG_} ${LOG}
+
+	echo "${post_cmd}" >> ${LOG_} ${LOG}
+
+
+	http_code=$(tail -n1 <<< "${post_cmd}")
+	response=$(echo -e "${post_cmd}" | tail -2 | head -1)
+
+	echo "Response is ${response}" >> ${LOG_} ${LOG}
+	echo "http_code is $http_code Response is ${response}"
+}
+
